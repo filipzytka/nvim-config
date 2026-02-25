cc335802944fcb2f4b4388898552d746c6bdc45f
filipzytka <filip.zytka@gmail.com>
2026-02-25 14:06:32 +0100

    mapping for switching windows

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,16 +32,20 @@ vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- key mappings
 vim.g.mapleader = " "
-local keymap = vim.keymap 
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("n", "<leader>h", ":nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("n", "<leader>h", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+vim.keymap.set('n', "<C-h>", "<C-w>h" ,{noremap=true})
+vim.keymap.set('n', "<C-j>", "<C-w>j" ,{noremap=true})
+vim.keymap.set('n', "<C-k>", "<C-w>k" ,{noremap=true})
+vim.keymap.set('n', "<C-l>", "<C-w>l" ,{noremap=true})
 
 -- Setup lazy.nvim
 require("lazy").setup({
