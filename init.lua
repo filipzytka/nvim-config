6ba22a74eec13adc447c18268431edf4dcdd1d8f
filipzytka <filip.zytka@gmail.com>
2026-05-16 13:15:01 +0200

    xd

--- init.lua
+++ init.lua
@@ -1,4 +1,127 @@
-require("config.lazy")
+-- lazy configuration
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- plugins
+plugins = {
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+				},
+
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					pattern = "*",
+					callback = function(args)
+						require("conform").format({ bufnr = args.buf })
+					end,
+				}),
+			})
+		end,
+	},
+
+	{
+		"metalelf0/black-metal-theme-neovim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("black-metal").setup({
+				-- optional configuration here
+			})
+			require("black-metal").load()
+		end,
+	},
+	{
+		-- autopairs { brackets }
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		-- gitsigns
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged_enable = true,
+				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+				numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				watch_gitdir = {
+					follow_files = true,
+				},
+				auto_attach = true,
+				attach_to_untracked = false,
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+					use_focus = true,
+				},
+				current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+				blame_formatter = nil, -- Use default
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil, -- Use default
+				max_file_length = 40000, -- Disable if file is longer than this (in lines)
+				preview_config = {
+					-- Options passed to nvim_open_win
+					style = "minimal",
+					relative = "cursor",
+					row = 0,
+					col = 1,
+				},
+			})
+			require("gitsigns").load()
+		end,
+	},
+}
+
+-- plugin configurations
+
+require("lazy").setup({
+	spec = { plugins },
+	{ import = "plugins" },
+	install = { colorscheme = { "habamax" } },
+	checker = { enabled = true },
+})
+
+-- vim specific configurations & key binds
 
 vim.cmd("let g:netrw_liststyle = 3")
 vim.opt.clipboard:append("unnamedplus")
@@ -15,40 +138,35 @@ vim.opt.cursorline = true
 vim.opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- key mappings
 vim.g.mapleader = " "
 
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 vim.keymap.set("n", "<leader>h", ":nohl<CR>", { desc = "Clear search highlights" })
--- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-vim.keymap.set('n', "<C-h>", "<C-w>h" ,{noremap=true})
-vim.keymap.set('n', "<C-j>", "<C-w>j" ,{noremap=true})
-vim.keymap.set('n', "<C-k>", "<C-w>k" ,{noremap=true})
-vim.keymap.set('n', "<C-l>", "<C-w>l" ,{noremap=true})
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
 
-vim.keymap.set('n', '<leader>f', ':find ', { desc = "Find File" })
-vim.keymap.set('n', '<leader>s', ':silent grep! <cword> | copen<CR>')
-vim.keymap.set('n', ']q', ':cnext<CR>')
-vim.keymap.set('n', '[q', ':cprev<CR>')
+vim.keymap.set("n", "<leader>f", ":find ", { desc = "Find File" })
+vim.keymap.set("n", "<leader>s", ":silent grep! <cword> | copen<CR>")
+vim.keymap.set("n", "]q", ":cnext<CR>")
+vim.keymap.set("n", "[q", ":cprev<CR>")
+
+vim.keymap.set("n", "<leader>E", ":Explore <CR>")
 
 vim.opt.path:append("**")
 vim.opt.wildmode = "longest:full,full"
 
--- grep
 vim.opt.grepprg = "rg --vimgrep -uu"
 vim.opt.grepformat = "%f:%l:%c:%m"
 
-vim.keymap.set('n', '<leader>g', function()
-    local search = vim.fn.input("Search for: ")
-    
-    if search ~= "" then
-        vim.cmd("silent grep! " .. search)
-        vim.cmd("copen")
-        vim.cmd("redraw!")
-    end
+vim.keymap.set("n", "<leader>g", function()
+	local search = vim.fn.input("Search for: ")
+
+	if search ~= "" then
+		vim.cmd("silent grep! " .. search)
+		vim.cmd("copen")
+		vim.cmd("redraw!")
+	end
 end)
